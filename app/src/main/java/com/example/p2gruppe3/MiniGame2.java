50f4f24478dcd08acda0f6988e2fb6c542da205f
Gucci HO <tho22@student.aau.dk>
2023-04-25 13:43:53 +0200

    moldButton1 functionality test

--- app/src/main/java/com/example/p2gruppe3/MiniGame2.java
+++ app/src/main/java/com/example/p2gruppe3/MiniGame2.java
@@ -3,14 +3,31 @@ package com.example.p2gruppe3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MiniGame2 extends AppCompatActivity {
+
+    private int numberOfMoldRemoved;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mini_game2);
+
+        ImageButton moldButton1 = (ImageButton)findViewById(R.id.moldButton1);
+
+        moldButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setNumberOfMoldRemoved(1);
+            }
+        });
     }
 
-    
+    public void setNumberOfMoldRemoved(int numberOfMoldRemoved) {
+        this.numberOfMoldRemoved++;
+    }
+
+
 }
