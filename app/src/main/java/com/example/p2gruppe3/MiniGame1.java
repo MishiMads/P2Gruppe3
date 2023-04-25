2f34ef4328ee9259704bf12d7db94b4e5ddab921
AndreasHJ3412 <ajorge22@student.aau.dk>
2023-04-25 13:53:54 +0200

    Start på mini game 1

--- app/src/main/java/com/example/p2gruppe3/MiniGame1.java
+++ app/src/main/java/com/example/p2gruppe3/MiniGame1.java
@@ -3,12 +3,38 @@ package com.example.p2gruppe3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MiniGame1 extends AppCompatActivity {
-
+private static ImageView imageview;
+    private static Button button;
+    private int currentImage;
+    int[] images={R.drawable.group_2,R.drawable.group_5};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mini_game1);
+        buttonclick();
     }
+
+    public void buttonclick() {
+        imageview=(ImageView)findViewById(R.id.nose);
+        button=(Button)findViewById(R.id.button);
+
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        currentImage++;
+                        currentImage=currentImage % images.length;
+                        imageview.setImageResource(images[currentImage]);
+                    }
+                }
+        );
+
+    }
+
+
 }
