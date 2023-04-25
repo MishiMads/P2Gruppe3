d32f8339045fc4f6b96ef7373e2e9d0d339a5e31
Tallest boi of all (Markus) <mflens22@student.aau.dk>
2023-04-25 14:04:26 +0200

    Forside map added og kan dragges

--- app/src/main/java/com/example/p2gruppe3/MainActivity.java
+++ app/src/main/java/com/example/p2gruppe3/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.p2gruppe3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView Map;
+    float xDown = 0, yDown = 0;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map = findViewById(R.id.MapImage);
+
+        Map.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                switch (motionEvent.getActionMasked()){
+
+                    case MotionEvent.ACTION_DOWN:
+                        xDown = motionEvent.getX();
+                        yDown = motionEvent.getY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        float xMoved, yMoved;
+                        xMoved = motionEvent.getX();
+                        yMoved = motionEvent.getY();
+
+                        float distanceX = xMoved - xDown;
+                        float distanceY = yMoved - yDown;
+
+                        Map.setX(Map.getX()+distanceX);
+                        Map.setY(Map.getY()+distanceY);
+
+                        break;
+                }
+
+
+                return true;
+            }
+        });
+
     }
+
+
 }
