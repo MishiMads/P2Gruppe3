821fdda459194d26c3367e6cae8bc6c970587f4d
Tallest boi of all (Markus) <mflens22@student.aau.dk>
2023-04-25 10:27:48 +0200

    MiniGames added

---
+++ app/src/main/java/com/example/p2gruppe3/MiniGame3.java
@@ -0,0 +1,14 @@
+package com.example.p2gruppe3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MiniGame3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mini_game3);
+    }
+}
