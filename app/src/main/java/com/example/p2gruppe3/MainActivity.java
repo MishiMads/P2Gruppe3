ed87e1ea244ac495a55f0ed7f7b620aa7a438cfa
Tallest boi of all (Markus) <mflens22@student.aau.dk>
2023-04-28 09:54:39 +0200

    Main activity forside drag map

--- app/src/main/java/com/example/p2gruppe3/MainActivity.java
+++ app/src/main/java/com/example/p2gruppe3/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     float xDown = 0, yDown = 0;
     private float limitX = 10;
     private float limitY = 10;
+    private int parentWidth = 0;
+    private int parentHeight = 0;
+
+
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -27,7 +32,48 @@ public class MainActivity extends AppCompatActivity {
 
         Map = findViewById(R.id.MapImage);
 
+        Map.post(new Runnable() {
+            @Override
+            public void run() {
+                // Get the width and height of the parent view
+                parentWidth = ((View) Map.getParent()).getWidth();
+                parentHeight = ((View) Map.getParent()).getHeight();
+            }
+        });
+
         Map.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                switch (motionEvent.getActionMasked()){
+
+                    case MotionEvent.ACTION_DOWN:
+                        xDown = motionEvent.getX();
+                        yDown = motionEvent.getY();
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        float xMoved, yMoved;
+                        xMoved = motionEvent.getX();
+                        yMoved = motionEvent.getY();
+
+                        float distanceX = xMoved - xDown;
+                        float distanceY = yMoved - yDown;
+
+                        Map.setX(Map.getX()+distanceX);
+                        Map.setY(Map.getY()+distanceY);
+
+
+
+
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+       /* Map.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
 
@@ -48,14 +94,35 @@ public class MainActivity extends AppCompatActivity {
                         Map.setX(Map.getX()+distanceX);
                         Map.setY(Map.getY()+distanceY);
 
+                        float newX = Map.getX() + distanceX;
+                        float newY = Map.getY() + distanceY;
+
+                        if (newX < limitX) {
+                            newX = limitX;
+                        } else if (newX > parentWidth - Map.getWidth() - limitX) {
+                            newX = parentWidth - Map.getWidth() - limitX;
+                        }
+
+                        if (newY < limitY) {
+                            newY = limitY;
+                        } else if (newY > parentHeight - Map.getHeight() - limitY) {
+                            newY = parentHeight - Map.getHeight() - limitY;
+                        }
+
+
+                        Map.setX(newX);
+                        Map.setY(newY);
+
                         break;
                 }
+
 
 
                 return true;
             }
         });
 
+    }*/
     }
 
 
